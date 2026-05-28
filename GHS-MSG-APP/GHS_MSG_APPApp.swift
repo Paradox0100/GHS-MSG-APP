d6991e7f4f9ac22865e70fcb40c29f5990f73a9d
Paradox0100 <milo.saucier@gmail.com>
2026-05-28 08:25:31 -0400

    Initial Commit

---
+++ GHS-MSG-APP/GHS_MSG_APPApp.swift
@@ -0,0 +1,32 @@
+//
+//  GHS_MSG_APPApp.swift
+//  GHS-MSG-APP
+//
+//  Created by Student on 5/28/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct GHS_MSG_APPApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
