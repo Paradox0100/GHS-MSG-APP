e980ae7625e4581aa2f8bbca24b3d0eb6c91e3b2
oemerson8158 <oemerson8158@hotmail.com>
2026-06-05 08:06:04 -0400

    Enlarged title for settings UI

--- GHS-MSG-APP/SettingsUI.swift
+++ GHS-MSG-APP/SettingsUI.swift
@@ -12,7 +12,9 @@ struct SettingsUI: View {
     //Toggle Variables
     
     @State private var notificationsEnabled = true //variable for notifacations toggle
+    
     @State private var darkModeEnabled = false //variable for dark mode toggle
+    
     @State private var rememberMeEnabled = false //variable for remeber me toggle
     
     //Dropdown Variables
@@ -24,6 +26,7 @@ struct SettingsUI: View {
     @State private var fontSize: Double = 20.0 //variable for font size slider
     
     //Background Variables
+    
     private var backgroundGradient: some View {
         LinearGradient(
             colors: [
@@ -40,84 +43,86 @@ struct SettingsUI: View {
     
     var body: some View {
                     
-            NavigationStack {
+        NavigationStack {
+            
+            Form {
                 
-                Form {
-                    
-                    Section {
-                        HStack {
-                            Toggle("Enable Notifications", isOn: $notificationsEnabled)
-                                .font(.title)
-                                .padding()
-                        }
+                
+                Section {
+                    HStack {
+                        Toggle("Enable Notifications", isOn: $notificationsEnabled)
+                            .font(.title)
+                            .padding()
                     }
-                    
-                    Section {
-                        HStack {
-                            Toggle("Enable Dark Mode", isOn: $darkModeEnabled)
-                                .font(.title)
-                                .padding()
-                        }
+                }
+                
+                Section {
+                    HStack {
+                        Toggle("Enable Dark Mode", isOn: $darkModeEnabled)
+                            .font(.title)
+                            .padding()
                     }
-                    
-                    Section {
-                        HStack {
-                            Toggle("Enable Remember Me", isOn: $rememberMeEnabled)
-                                .font(.title)
-                                .padding()
-                        }
+                }
+                
+                Section {
+                    HStack {
+                        Toggle("Enable Remember Me", isOn: $rememberMeEnabled)
+                            .font(.title)
+                            .padding()
                     }
-                    
-                    Section {
-                        DisclosureGroup("Accessibility", isExpanded: $isExpanded) {
+                }
+                
+                Section {
+                    DisclosureGroup("Accessibility", isExpanded: $isExpanded) {
+                        
+                        VStack {
                             
-                            VStack {
-                                
-                                HStack(spacing: 15){
-                                    Text("Font Size")
-                                        .font(.title2)
-                                    Spacer()
-                                }
-                                .padding()
-                                
-                                HStack(spacing: 15) {
-                                    Slider(value: $fontSize, in: 10...30) {
-                                        Text("Font Size Slider")
-                                    } minimumValueLabel: {
-                                        Text("10pt")
-                                            .font(.title2)
-                                    } maximumValueLabel: {
-                                        Text("30pt")
-                                            .font(.title2)
-                                    }
-                                }
-                                .padding()
-                                
+                            HStack(spacing: 15){
+                                Text("Font Size")
+                                    .font(.title2)
+                                Spacer()
                             }
+                            .padding()
                             
+                            HStack(spacing: 15) {
+                                Slider(value: $fontSize, in: 10...30) {
+                                    Text("Font Size Slider")
+                                } minimumValueLabel: {
+                                    Text("10pt")
+                                        .font(.title2)
+                                } maximumValueLabel: {
+                                    Text("30pt")
+                                        .font(.title2)
+                                }
+                            }
+                            .padding()
                         }
                     }
-                    
-                    .font(.title)
-                    .padding()
-                    
                 }
                 
-                .navigationBarTitleDisplayMode(.inline)
-                .toolbar {
-                    ToolbarItem(placement: .principal) {
-                        Text("Settings")
-                            .font(.largeTitle)
-                            .bold()
-                            .foregroundColor(.white)
-                    }
-                }
+                .font(.title)
+                .padding()
                 
-                .scrollContentBackground(.hidden)
-                .background(backgroundGradient.ignoresSafeArea())
-                
-
             }
+ 
+//Uncomment to lock title scrolling
+//            .scrollDisabled(true)
+            
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .principal) {
+                    Text("Settings")
+                        .font(.system(size: 48))
+                        .bold()
+                        .foregroundColor(.white)
+                }
+            }
+            
+            .scrollContentBackground(.hidden)
+            .background(backgroundGradient.ignoresSafeArea())
+            
+            
+        }
     }
 }
 
